1e2d1fdb4c3287eb38f14bfdafecf56b61422217
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2024-04-01 12:53:05 +0300

    Added Tuple class from Python’s typing module

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """index_range that takes two integer arguments page and page_size """
+from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
