2010bd7c2928471810fbcab3bd142842dd56e451
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2024-04-01 13:11:55 +0300

    Update 1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -10,7 +10,7 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     return type expectation: tuple
     """
     start_index = (page - 1) * page_size
-    end_index = page * page_size
+    end_index = page + page_size
     return start_index, end_index
     
 class Server:
