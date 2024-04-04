08b39f082361dc02ce6c6c4fb491cc222eea6325
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2024-04-04 04:30:17 +0300

    Updated documentation

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
-""" Caching system. """
-
+"""Basic caching module.
+"""
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """ Class inhereted from BaseCaching"""
-
+    """ Implements basic caching system.
+    """
     def put(self, key, item):
-    """ Add an item in the cache. """
-        if not (key is None or item is None):
-            self.cache_data[key] = item
-
+        """ Add item in the cache.
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
 
     def get(self, key):
-    """ Get an item by key. """
-    if key is None or key not in self.cache_data:
-        return None
-    return self.cache_data.get(key)
+        """ Retrieve an item by key.
+        """
+        return self.cache_data.get(key, None)
