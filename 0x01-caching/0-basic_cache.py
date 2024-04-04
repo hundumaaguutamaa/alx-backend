6d4f969d3a82a1834d0a86213e1f457baf73c5e1
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2024-04-04 04:21:14 +0300

    Conditional statement updated

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -9,10 +9,12 @@ class BasicCache(BaseCaching):
 
     def put(self, key, item):
     """ Add an item in the cache. """
-    if all([key, item]):
-        self.cache_data[key] = item
+    if not (key is None or item is None):
+            self.cache_data[key] = item
 
 
     def get(self, key):
     """ Get an item by key. """
-    return self.cache_data.get(key) if all([key, key in self.cache_data]) else None
+    if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data.get(key)
