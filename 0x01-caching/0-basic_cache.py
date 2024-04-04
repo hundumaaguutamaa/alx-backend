9d4cc6820d2003862b0268467d2fa6ac788316c9
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2024-04-04 04:25:40 +0300

    Updated space

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -9,12 +9,12 @@ class BasicCache(BaseCaching):
 
     def put(self, key, item):
     """ Add an item in the cache. """
-    if not (key is None or item is None):
+        if not (key is None or item is None):
             self.cache_data[key] = item
 
 
     def get(self, key):
     """ Get an item by key. """
     if key is None or key not in self.cache_data:
-            return None
-        return self.cache_data.get(key)
+        return None
+    return self.cache_data.get(key)
