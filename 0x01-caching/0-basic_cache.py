ce02fa82e40c07f5cadcf83b73586590b0a262c3
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2024-07-25 03:06:06 +0300

    Updated functions

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -3,7 +3,27 @@
 Base caching module
 """
 
-from base_caching import BaseCaching
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
+    """
+    Inherited BaseCaching
+    """
+
+    def put(self, key, item):
+        """
+        Put key and data
+        """
+        if key is None or item is None:
+            pass
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Return value of key
+        """
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data.get(key)
