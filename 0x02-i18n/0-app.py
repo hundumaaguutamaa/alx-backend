b84a7f3e2404945ee47af3011eea7444602d1e94
hundumaaguutamaa <hundumaa.guutamaa@gmail.com>
2024-07-30 18:33:17 +0000

    0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,1 +1,22 @@
 #!/usr/bin/env python3
+"""
+Flask application that renders a "Hello world" message.
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index() -> str:
+    """
+    Render the index.html template.
+
+    Returns:
+        str: The rendered HTML content.
+    """
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
