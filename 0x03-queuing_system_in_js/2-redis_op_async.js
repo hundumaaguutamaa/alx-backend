df8f9dee412a83ff2573603791b88d8857838b79
hundumaaguutamaa <hundumaa.guutamaa@gmail.com>
2024-08-28 08:15:27 +0000

    Node Redis client and async operations

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,35 @@
+import { createClient, print } from 'redis';
+import { promisify } from 'util';
+
+const client = createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+client.connect();
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, print);
+}
+
+const getAsync = promisify(client.get).bind(client);
+
+async function displaySchoolValue(schoolName) {
+  try {
+    const value = await getAsync(schoolName);
+    console.log(value);
+  } catch (err) {
+    console.log(`Error: ${err.message}`);
+  }
+}
+
+// Call the functions
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
+
