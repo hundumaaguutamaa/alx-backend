98cfa69cc6eaaac0a9fc24262d21f6dd1c9c5081
hundumaaguutamaa <hundumaa.guutamaa@gmail.com>
2024-08-28 09:08:17 +0000

    Track progress and errors with Kue: Create the Job processor

---
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -0,0 +1,25 @@
+const kue = require('kue');
+const queue = kue.createQueue();
+
+// Blacklisted phone numbers
+const blacklistedNumbers = ['4153518780', '4153518781'];
+
+// Function to send notification
+function sendNotification(phoneNumber, message, job, done) {
+    job.progress(0, 100);
+
+    if (blacklistedNumbers.includes(phoneNumber)) {
+        return done(new Error(`Phone number ${phoneNumber} is blacklisted`));
+    }
+
+    job.progress(50, 100);
+    console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+    done();
+}
+
+// Process jobs in the 'push_notification_code_2' queue
+queue.process('push_notification_code_2', 2, (job, done) => {
+    const { phoneNumber, message } = job.data;
+    sendNotification(phoneNumber, message, job, done);
+});
+
