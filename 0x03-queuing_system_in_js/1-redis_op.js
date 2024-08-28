fedc81c6ded5a326925de73e6b137b8a7a5fbf8b
hundumaaguutamaa <hundumaa.guutamaa@gmail.com>
2024-08-28 08:12:58 +0000

    Redis operation

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,33 @@
+import { createClient, print } from 'redis';
+
+const client = createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+client.connect();
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, print);
+}
+
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (err, reply) => {
+    if (err) {
+      console.log(`Error: ${err.message}`);
+    } else {
+      console.log(reply);
+    }
+  });
+}
+
+// Call the functions
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
+
